b47220743c8f5757f117d095398ecb0a0554ac7b
maiminhtien94 <maiminhtien1994@gmail.com>
2023-07-03 23:37:50 +0700

    Fish push

---
+++ language-test/language-test/Language.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace languagetest {
+	class ILanguage {
+		virtual void LanguageChange(double val) = 0;
+	};
+	class Language {
+	public:
+
+	private:
+
+	public:
+
+	private:
+
+	};
+}
